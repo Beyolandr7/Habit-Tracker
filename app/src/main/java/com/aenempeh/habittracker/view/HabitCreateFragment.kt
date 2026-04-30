f66649f5b5aaeb9bccb0b0e5c593e96b8f02adcb
EylinJois <s160423031@student.ubaya.ac.id>
2026-04-30 13:58:35 +0700

    Added HabitViewModel & created UI for create habit

--- app/src/main/java/com/aenempeh/habittracker/view/HabitCreateFragment.kt
+++ app/src/main/java/com/aenempeh/habittracker/view/HabitCreateFragment.kt
@@ -5,14 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.aenempeh.habittracker.R
+import com.aenempeh.habittracker.databinding.ActivityMainBinding
+import com.aenempeh.habittracker.databinding.FragmentHabitCreateBinding
+import com.aenempeh.habittracker.viewmodel.HabitViewModel
 
 class HabitCreateFragment : Fragment() {
+    private lateinit var binding: FragmentHabitCreateBinding
+    private lateinit var viewModel: HabitViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_habit_create, container, false)
+        binding = FragmentHabitCreateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[HabitViewModel::class.java]
+
+        binding.btnSubmit.setOnClickListener {
+            val name = binding.txtName.text.toString()
+            val desc = binding.txtDescription.text.toString()
+            val goal = binding.txtGoal.text.toString().toIntOrNull() ?: 0
+            val unit = binding.txtUnit.text.toString()
+            val icon = binding.pickIcon.selectedItem.toString()
+
+            viewModel.createHabit(name, desc, goal, unit, icon)
+        }
     }
 }
